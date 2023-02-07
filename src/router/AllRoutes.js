542eaa56619f4d988a2ef25a0f56b4c689cedb5f
Noemiegrau <noemiegrau@outlook.fr>
2023-02-07 14:15:12 -0800

    TTD

--- src/router/AllRoutes.js
+++ src/router/AllRoutes.js
@@ -1,105 +1,32 @@
 import React from "react";
-// import Preview from "../views/Preview";
+import Preview from "../views/Preview";
 // import HomeLightAnimation from "../views/all-home-version/HomeLightAnimation";
 // import HomeTyperCreative from "../views/all-home-version/HomeTyperCreative";
 // import HomeDarkAnimation from "../views/all-home-version/HomeDarkAnimation";
 import MainPage from "../views/all-home-version/MainPage";
-import NotFound from "../views/NotFound";
-import { Routes, Route, HashRouter } from "react-router-dom";
+// import NotFound from "../views/NotFound";
+import { Routes, Route } from "react-router-dom";
 import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
-
-//test
 
 const AllRoutes = () => {
   return (
-    // <>
-
-    <HashRouter>
+    <>
       <ScrollTopBehaviour />
       <Routes>
         {/* <Route exact
                 path={ `${ process.env.PUBLIC_URL + "/" }` }
                 element={ Preview } /> */}
-            <Route path="/" element={<MainPage />} />
+            <Route path="*" element={<MainPage />} />
         {/* <Route path="/" element={<Preview />} /> */}
         {/* <Route path="/home-light-animation" element={<HomeLightAnimation />} />
         <Route path="/home-dark-animation" element={<HomeDarkAnimation />} />
         <Route path="/home-typer-creative" element={<HomeTyperCreative />} />
         <Route path="/home-parallax-typer" element={<MainPage />} /> */}
         {/* <Route path="/Home" element={<MainPage />} /> */}
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
-      </HashRouter>
-    // </>
+    </>
   );
 };
 
 export default AllRoutes;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import Preview from "../views/Preview";
-// // import HomeLightAnimation from "../views/all-home-version/HomeLightAnimation";
-// // import HomeTyperCreative from "../views/all-home-version/HomeTyperCreative";
-// // import HomeDarkAnimation from "../views/all-home-version/HomeDarkAnimation";
-// import MainPage from "../views/all-home-version/MainPage";
-// // import NotFound from "../views/NotFound";
-// import { Routes, Route } from "react-router-dom";
-// import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
-
-// const AllRoutes = () => {
-//   return (
-//     <>
-//       <ScrollTopBehaviour />
-//       <Routes>
-//         {/* <Route exact
-//                 path={ `${ process.env.PUBLIC_URL + "/" }` }
-//                 element={ Preview } /> */}
-//             <Route path="*" element={<MainPage />} />
-//         {/* <Route path="/" element={<Preview />} /> */}
-//         {/* <Route path="/home-light-animation" element={<HomeLightAnimation />} />
-//         <Route path="/home-dark-animation" element={<HomeDarkAnimation />} />
-//         <Route path="/home-typer-creative" element={<HomeTyperCreative />} />
-//         <Route path="/home-parallax-typer" element={<MainPage />} /> */}
-//         {/* <Route path="/Home" element={<MainPage />} /> */}
-//         {/* <Route path="*" element={<NotFound />} /> */}
-//       </Routes>
-//     </>
-//   );
-// };
-
-// export default AllRoutes;
