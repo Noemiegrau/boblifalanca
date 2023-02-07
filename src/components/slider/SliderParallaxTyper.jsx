416930a003f9c445c271d77ebaec80e5b11061e2
Noemiegrau <noemiegrau@outlook.fr>
2023-02-06 20:55:44 -0800

    initial commit

---
+++ src/components/slider/SliderParallaxTyper.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import { Parallax } from "react-parallax";
+import ReactTyped from "react-typed";
+import SocialTwo from "../SocialTwo";
+
+const image1 = "img/slider/10.jpg";
+
+const SliderParallaxTyper = () => {
+  return (
+    <>
+      <Parallax bgImage={image1} strength={500}>
+        <div className="beny_tm_hero " id="home">
+          <div className="background d-none d-sm-block">
+            <div
+              className="image"
+              style={{
+                backgroundImage: `url(${
+                  process.env.PUBLIC_URL + "img/slider/10.jpg"
+                })`,
+              }}
+            ></div>
+            <div className="overlay"></div>
+          </div>
+          {/* End bg */}
+          <div className="go-to go-to-next">
+            <a href="#about">
+              <span></span>
+            </a>
+          </div>
+          {/* End animated goto button */}
+          <div className="container">
+            <div className="content">
+              <div className="content_inner">
+                <h3
+                  className="name"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                >
+                  Site Officiel 
+                </h3>
+                <h1
+                  className="job"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="50"
+                >
+                  Bob Lifalanca
+                </h1>
+                <h2
+                  className="typer"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="100"
+                >
+                  <ReactTyped
+                    loop
+                    typeSpeed={100}
+                    backSpeed={60}
+                    strings={[
+                      "Videographe",
+                      "Youtubeur",
+                      "TikTokeur",
+                    ]}
+                    smartBackspace
+                    shuffle={false}
+                    backDelay={1}
+                    fadeOut={false}
+                    fadeOutDelay={100}
+                    loopCount={0}
+                    showCursor
+                    cursorChar="|"
+                  />
+                </h2>
+
+                <p
+                  className="text"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="150"
+                >
+                  
+                  Photographe et vidéaste parisien, je suis spécialisé dans l'événementiel, 
+                  ainsi que les publicités pour les marques. Je couvres vos événements, anniversaires, 
+                  mariages et soirées pour immortaliser vos moments les plus chers. 
+                  Mon travail permet aux clients d'augmenter leurs ventes et de construire une meilleure marque avec un retour sur investissement positif.
+                </p>
+
+                <div
+                  className="beny_tm_button"
+                  data-aos="fade-up"
+                  data-aos-duration="1200"
+                  data-aos-delay="200"
+                >
+                  <a className="anchor" href="#about">
+                    <span className="wrapper">
+                      <span className="first">Voir mon travail</span>
+                      <span className="second">Voir mon travail</span>
+                    </span>
+                  </a>
+                </div>
+                {/* End beny_tm_button */}
+              </div>
+            </div>
+          </div>
+          <div className="social">
+            <SocialTwo />
+          </div>
+        </div>
+      </Parallax>
+    </>
+  );
+};
+
+export default SliderParallaxTyper;
---
+++ src/router/AllRoutes.js
@@ -0,0 +1,28 @@
+import React from "react";
+// import Preview from "../views/Preview";
+// import HomeLightAnimation from "../views/all-home-version/HomeLightAnimation";
+// import HomeTyperCreative from "../views/all-home-version/HomeTyperCreative";
+// import HomeDarkAnimation from "../views/all-home-version/HomeDarkAnimation";
+import MainPage from "../views/all-home-version/MainPage";
+import NotFound from "../views/NotFound";
+import { Routes, Route } from "react-router-dom";
+import ScrollTopBehaviour from "../components/ScrollTopBehaviour";
+
+const AllRoutes = () => {
+  return (
+    <>
+      <ScrollTopBehaviour />
+      <Routes>
+        {/* <Route path="/" element={<Preview />} /> */}
+        {/* <Route path="/home-light-animation" element={<HomeLightAnimation />} />
+        <Route path="/home-dark-animation" element={<HomeDarkAnimation />} />
+        <Route path="/home-typer-creative" element={<HomeTyperCreative />} />
+        <Route path="/home-parallax-typer" element={<MainPage />} /> */}
+        <Route path="/" element={<MainPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
+
+export default AllRoutes;
